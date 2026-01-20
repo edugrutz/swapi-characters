d0fb842ef7380d4297784010c7bd7bae7b905b94
Eduardo <grutzmann9@gmail.com>
2026-01-20 16:26:22 -0300

    fix: remove old css import

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ConfigProvider, theme } from "antd";
 import "antd/dist/reset.css";
-import "./index.css";
 import App from "./App.tsx";
 
 const queryClient = new QueryClient();
