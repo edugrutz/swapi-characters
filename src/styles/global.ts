630f83c391ccd437d37eb4bd44158c510a898738
Eduardo <grutzmann9@gmail.com>
2026-01-20 16:26:58 -0300

    fix(ui): add body padding and min height

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,4 +3,16 @@ import { antdStyles } from "./antd/antd";
 
 export const GlobalStyles = createGlobalStyle`
     ${antdStyles}
+
+    body {
+        min-height: 100vh;
+        background-color: #000000ff;
+        padding: 24px;
+    }
+    
+    @media (max-width: 768px) {
+        body {
+            padding: 12px;
+        }
+    }
 `;
